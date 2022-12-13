c3fe64f9153c2335aea90ddf12b1db5904b68d5a
Guilherme Figueira <79333503+Alarket@users.noreply.github.com>
2022-12-13 20:31:05 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,20 @@
-let express = require('express');
-let app = express();
-console.log("Hello World");
+var express = require('express');
+var app = express();
+console.log("Hello World")
+
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+ '/views/index.html');
+  app.use(express.static(__dirname + "/public"));
+})
+app.get("/json", (req, res) => {
+     if(process.env.MESSAGE_STYLE === "uppercase"){
+       res.json({"message" : "HELLO JSON"});
+     }
+     res.json({"message" : "Hello json"});
+   });
+module.exports = app;
 
 
 
